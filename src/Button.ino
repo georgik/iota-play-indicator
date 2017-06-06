2d38917ebab6cf82641d66ff084d4169b6f972dc
Juraj Michalek <juraj.michalek@gmail.com>
2017-06-06 21:36:50 +0200

    add specific version of RFM69

--- src/Button.ino
+++ src/Button.ino
@@ -1,32 +1,78 @@
-/*
-  Button example
- Turns on and off a light emitting diode(LED) D6 when pressing a pushbutton attached to pin 2.
- */
-// set pin numbers:
-const int buttonPin = 30;     // the number of the pushbutton pin
-const int ledPin =  26;      // the number of the LED pin
+#include "YiPlayRF.h"
+#include <Wire.h>
 
+#define Addr 0x40
 
+const int buttonPin = 30;
+const int ledPin =  26;
+const int ledPin2 =  25;
 int buttonState = 0;
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
+  // put your setup code here, to run once:
   pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+
+  //configure radio - network 66, node 1
+  setupRadio(66, 1, NULL);
+  Wire.begin();
+  Wire.beginTransmission(Addr);
+  // Send temp measurement command, NO HOLD MASTER
+  Wire.write(0xF3);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // temp msb, temp lsb
+  if (Wire.available() == 2)
+  {
+    Wire.read();
+    Wire.read();
+  }
 }
 
 void loop() {
-  // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, LOW);
-  } else {
-    // turn LED off:
+    // turn LED of:
     digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED on and start sending temperature
+    digitalWrite(ledPin, LOW);
+    byte data[2];
+    Wire.beginTransmission(Addr);
+    // Send temp measurement command, NO HOLD MASTER
+    Wire.write(0xF3);
+    // Stop I2C transmission
+    Wire.endTransmission();
+
+    delay(200);
+    // Request 2 bytes of data
+    Wire.requestFrom(Addr, 2);
+
+    // Read 2 bytes of data
+    // temp msb, temp lsb
+    if (Wire.available() == 2)
+    {
+      data[0] = Wire.read();
+      data[1] = Wire.read();
+    }
+    // Convert the data
+    float temp  = ((data[0] * 256.0) + data[1]);
+    float ctemp = ((175.72 * temp) / 65536.0) - 46.85;
+
+    //send data over radio
+    if (radioSendTemp(2, data)) {
+      //when recieved, blink
+      digitalWrite(ledPin2, LOW);
+      delay(100);
+      digitalWrite(ledPin2, HIGH);
+    }
+
   }
 }
